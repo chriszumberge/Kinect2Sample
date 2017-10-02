bf7c9ecbcfb05566f637cdb9edde5b5a4ae30c82
chris <chris@DESKTOP-F4DMQ0I>
2017-10-02 17:33:59 -0400

    Lab 1 Complete

---
+++ Kinect2Sample/Kinect2Sample/Kinect2Sample.Windows/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using WindowsPreview.Kinect;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Kinect2Sample
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private KinectSensor kinectSensor = null;
+        
+        public MainPage()
+        {
+            // one sensor is currently supported 
+            this.kinectSensor = KinectSensor.GetDefault();
+            // open the sensor
+            this.kinectSensor.Open();
+
+            this.InitializeComponent();
+        }
+    }
+}
